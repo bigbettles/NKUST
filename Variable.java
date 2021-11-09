850f7a5656fafc1bbaa1f52292c0e7662cb75d0d
bigbettles <c110181102@nkust.edu.tw>
2021-11-09 16:33:04 +0800

    This is my first commit

---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        // This is a comment
+        //lowerCamelCase variable name
+        String myName = "big beetle";
+        System.out.println("Hello " + myName);
+    }
+}
--- syste.java
+++ syste.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class syste {
+public class syste {
+    public static void main(String[] args) {
+        double a= 1.78 ;
+        int b= 54 ;
+        System.out.println(b/(a*a));
+}
 }
